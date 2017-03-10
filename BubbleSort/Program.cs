716fbf34879500dad7c4096c37b4c1c0504b7bc7
VDGone <disasterpiece788@gmail.com>
2017-03-10 12:08:50 +0200

    practice

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BubbleSort
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] numsToSort = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            bool swapped = true;
+            int temp;
+            int n = numsToSort.Length - 1;
+
+            while (swapped)
+            {
+                swapped = false;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (numsToSort[i] > numsToSort[i + 1])
+                    {
+                        temp = numsToSort[i];
+                        numsToSort[i] = numsToSort[i + 1];
+                        numsToSort[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                n -= 1;
+            }
+
+            Console.WriteLine(string.Join(", ", numsToSort));
         }
     }
 }
